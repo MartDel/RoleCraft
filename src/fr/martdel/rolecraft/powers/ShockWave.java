97fd27cf768db477402ccb035591ef6cda8fcfc8
Martin Delebecque <martin-delebecque@outlook.fr>
2020-12-27 19:43:41 +0100

    Prototype shock wave (failed)

--- src/fr/martdel/rolecraft/powers/ShockWave.java
+++ src/fr/martdel/rolecraft/powers/ShockWave.java
@@ -1,7 +1,10 @@
 package fr.martdel.rolecraft.powers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,17 +16,27 @@ import fr.martdel.rolecraft.RoleCraft;
 
 public class ShockWave {
 
-	private final int DELAY = 2;
+	private final int DELAY = 15;
 	private final int MAXRADIUS = 15;
 
 	private Location center;
 	private int radius = 1;
+	
+	private Map<Integer, Integer> forbidden_x;
+	private Map<Integer, Integer> forbidden_z;
+	private Map<Integer, Integer[]> forbidden_corners;
 
 	private RoleCraft plugin;
 	private BukkitScheduler scheduler;
 
 	public ShockWave(RoleCraft plugin, Location center) {
 		this.center = center;
+		System.out.println("Centre: x=" + center.getBlockX() + " z=" + center.getBlockZ());
+		
+		this.forbidden_x = new HashMap<>();
+		this.forbidden_z = new HashMap<>();
+		this.forbidden_corners = new HashMap<>();
+		
 		this.plugin = plugin;
 		this.scheduler = plugin.getServer().getScheduler();
 	}
@@ -32,6 +45,15 @@ public class ShockWave {
 		scheduler.runTaskLater(plugin, new Runnable() {
 			@Override
 			public void run() {
+				// Down last blocs
+				Location[] old_corners = getCorners(radius - 1);
+				for (int i = 0; i < old_corners.length; i++) {
+					List<Location> blocs = getBlocsForRow(old_corners[i], getRowLength(radius - 1), i);
+					for (Location bloc : blocs) {
+						if(checkRules(bloc)) bloc.getWorld().getBlockAt(bloc).setType(Material.AIR);
+					}
+				}
+				
 				// Up the blocs
 				if((int) radius != MAXRADIUS - 1) {
 					Location[] corners = getCorners(radius);
@@ -41,20 +63,13 @@ public class ShockWave {
 							Location under_bloc_loc = new Location(bloc.getWorld(), bloc.getBlockX(), bloc.getBlockY() - 1, bloc.getBlockZ());
 							Block under_bloc = under_bloc_loc.getWorld().getBlockAt(under_bloc_loc);
 							Material under_bloc_type = under_bloc.getType();
-							bloc.getWorld().getBlockAt(bloc).setType(under_bloc_type);
+							Block current_bloc = bloc.getWorld().getBlockAt(bloc);
+							if(addRules(bloc) && checkRules(bloc)) current_bloc.setType(under_bloc_type);
 						}
 					}
 				}
-				
-				// Down last blocs
-				Location[] old_corners = getCorners(radius - 1);
-				for (int i = 0; i < old_corners.length; i++) {
-					List<Location> blocs = getBlocsForRow(old_corners[i], getRowLength(radius - 1), i);
-					for (Location bloc : blocs) {
-						bloc.getWorld().getBlockAt(bloc).setType(Material.AIR);
-					}
-				}
 				radius++;
+				System.out.println(radius);
 				if(radius < MAXRADIUS) scheduler.runTaskLater(plugin, this, DELAY);
 			}
 		}, DELAY);
@@ -110,6 +125,100 @@ public class ShockWave {
 		}
 		
 		return blocs;
+	}
+	
+	private boolean addRules(Location bloc) {
+		Integer x = bloc.getBlockX();
+		Integer z = bloc.getBlockZ();
+		
+		// Check if the bloc is a forbidden bloc or not
+		Block b = bloc.getWorld().getBlockAt(bloc);
+		if(!b.getType().equals(Material.AIR)) {
+			// Add rule
+			System.out.println("Bloc détecté");
+			
+			// Check corners
+			Location[] corners = getCorners(radius);
+			for (int i = 0; i < corners.length; i++) {
+				if(corners[i].equals(bloc)) {
+					System.out.println("dans un coin : " + i + " " + radius);
+					Integer[] coord = {bloc.getBlockX() - center.getBlockX(), bloc.getBlockZ() - center.getBlockZ()};
+					forbidden_corners.put(i, coord);
+					System.out.println("Nouvelle règle : i=" + i + " x=" + coord[0] + " z=" + coord[1]);
+					return false;
+				}
+			}
+			
+			// Check x
+			if((z - center.getBlockZ()) > radius || (z - center.getBlockZ()) < (0 - radius)) {
+				System.out.println("en X");
+				Integer z_state = getZState(z, center.getBlockZ());
+				forbidden_x.put(x, z_state);				
+				return false;
+			}
+			
+			// Check z
+			if((x - center.getBlockX()) > radius || (x - center.getBlockX()) < (0 - radius)) {
+				System.out.println("en Z");
+				Integer x_state = getZState(x, center.getBlockX());
+				forbidden_z.put(z, x_state);
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean checkRules(Location bloc) {
+		Integer x = bloc.getBlockX();
+		Integer z = bloc.getBlockZ();
+		
+		// Check corners
+		Set<Integer> keys = forbidden_corners.keySet();
+		for (Integer i : keys) {
+			Integer[] coord = forbidden_corners.get(i);
+			switch (i) {
+			case 0: 
+				if((x - center.getBlockX()) <= coord[0] && (z - center.getBlockZ()) >= coord[1]) {
+					return false;
+				}
+				break;
+			case 1:
+				if((x - center.getBlockX()) >= coord[0] && (z - center.getBlockZ()) >= coord[1]) {
+					return false;
+				}
+				break;
+			case 2: 
+				if((x - center.getBlockX()) >= coord[0] && (z - center.getBlockZ()) <= coord[1]) {
+					return false;	
+				}
+				break;
+			case 3: 
+				if((x - center.getBlockX()) <= coord[0] && (z - center.getBlockZ()) <= coord[1]) {
+					return false;
+				}
+				break;
+			}
+		}
+		
+		// Check x
+		if(forbidden_x.containsKey(x)) {
+			Integer z_state = getZState(z, center.getBlockZ());
+			if(forbidden_x.get(x).equals(z_state)) return false;
+		}
+		
+		// Check z
+		if(forbidden_z.containsKey(z)) {
+			Integer x_state = getZState(x, center.getBlockX());
+			if(forbidden_z.get(z).equals(x_state)) return false;
+		}
+		
+		return true;
+	}
+	
+	private Integer getZState(Integer z, Integer playerZ) {
+		if(z - playerZ > 0) return 1;
+		else if (z - playerZ < 0) return -1;
+		return 0;
 	}
 
 }
