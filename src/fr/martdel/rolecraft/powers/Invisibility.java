d1eb892789027cfd46b017037db06db57051dc3b
MartDel <martin-delebecque@outlook.fr>
2021-03-20 14:37:02 +0100

    Remove armor when invisibility is activated

--- src/fr/martdel/rolecraft/powers/Invisibility.java
+++ src/fr/martdel/rolecraft/powers/Invisibility.java
@@ -4,10 +4,14 @@ import fr.martdel.rolecraft.RoleCraft;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.List;
 
 public class Invisibility {
 
@@ -15,7 +19,7 @@ public class Invisibility {
     public static final int COOLDOWN = RoleCraft.config.getInt("powers.invisibility.cooldown");
 
     private static final Material ITEMTYPE = RoleCraft.getConfigMaterial("powers.invisibility.item_type");
-    private static final String STARTMSG = RoleCraft.config.getString("powers.invisibility.start_msg");
+    private static final List<String> STARTMSG = RoleCraft.config.getStringList("powers.invisibility.start_msg");
     private static final String STOPMSG = RoleCraft.config.getString("powers.invisibility.stop_msg");
     private static final int DURATION = RoleCraft.config.getInt("powers.invisibility.duration");
 
@@ -27,13 +31,30 @@ public class Invisibility {
         this.player = player;
         this.plugin = rolecraft;
         this.scheduler = plugin.getServer().getScheduler();
-
     }
 
+    /**
+     * Turn invisibility on for the player during DURATION ticks
+     */
     public void start(){
+        // Start invisibility
         player.setInvisible(true);
-        assert STARTMSG != null;
-        player.sendMessage(STARTMSG);
+        for (String msg : STARTMSG){
+            player.sendMessage(msg);
+        }
+
+        // Hide player extra items
+        PlayerInventory inv = player.getInventory();
+        inv.setItem(9, inv.getHelmet());
+        inv.setItem(10, inv.getChestplate());
+        inv.setItem(11, inv.getLeggings());
+        inv.setItem(12, inv.getBoots());
+        inv.setItem(13, inv.getItemInOffHand());
+        inv.setArmorContents(new ItemStack[]{});
+        inv.setItemInOffHand(new ItemStack(Material.AIR));
+        player.updateInventory();
+
+        // Stop invisibility
         scheduler.runTaskLater(plugin, () -> {
             player.setInvisible(false);
             assert STOPMSG != null;
